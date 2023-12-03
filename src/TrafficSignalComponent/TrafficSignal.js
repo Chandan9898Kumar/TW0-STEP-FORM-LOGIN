a6e2ace42543e0c626a047713b2510a7d01e5598
Chandan <Chandan17ee@gmail.com>
2023-12-03 16:53:56 +0530

    chore:completed traffic signal

--- src/TrafficSignalComponent/TrafficSignal.js
+++ src/TrafficSignalComponent/TrafficSignal.js
@@ -1,8 +1,52 @@
+import "../App.css";
 import React, { useEffect, useState } from "react";
 
+const config = {
+  green: {
+    duration: 3000,
+    next: "yellow",
+  },
+  yellow: {
+    duration: 500,
+    next: "red",
+  },
+  red: {
+    duration: 4000,
+    next: "green",
+  },
+};
 const TrafficSignal = () => {
-  return(
-    <div style={{color:'red'}}>This is traffic signal</div>
+  const [streetColor, setStreetColor] = useState("green");
+
+  useEffect(() => {
+    const { duration, next } = config[streetColor];
+
+    let timer = setTimeout(() => {
+      setStreetColor(next);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [streetColor]);
+
+  return (
+    <>
+      <div className="MainHeading">
+        <div className="trafficHead">This is traffic signal</div>
+        <div className="trafficSignal">
+          <div
+            className={streetColor === "green" ? "green backGreen" : "green"}
+          ></div>
+
+          <div
+            className={
+              streetColor === "yellow" ? "yellow backYellow" : "yellow"
+            }
+          ></div>
+
+          <div className={streetColor === "red" ? "red backRed" : "red"}></div>
+        </div>
+      </div>
+    </>
   );
 };
 export default TrafficSignal;
