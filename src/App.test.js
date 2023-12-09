108138a7538e21c89d48f0cd355f4fc2e02817f3
Chandan <Chandan17ee@gmail.com>
2023-12-09 19:45:58 +0530

    chore: wrote test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, unmountComponentAtNode } from "@testing-library/react";
+import App from "./App";
+import HomePage from "./HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+describe("App component Testing", () => {
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  afterEach(() => {
+    // cleanup on exiting
+    // unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  test("Verify two step  text", () => {
+    act(() => {
+      render(
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>,
+        container
+      );
+    });
+    const linkElement = screen.getByText(/TwoStep/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test("Verify Login  text", () => {
+    act(() => {
+      render(
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>,
+        container
+      );
+    });
+    const linkElement = screen.getByText(/Login/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
